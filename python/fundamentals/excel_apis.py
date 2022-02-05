573f3707f7c8aaaaa3dfd371da51f18fc6bd9b4a
srmk <srikanth.mk@gmail.com>
2022-02-05 22:50:45 +0530

    Create excel_apis.py

---
+++ python/fundamentals/excel_apis.py
@@ -0,0 +1,24 @@
+import yfinance as yf
+import pandas as pd
+import matplotlib.pyplot as plt
+import xlwings as xw
+
+power = yf.Ticker("^NSEI")
+df = power.history(start="2020-01-01", end='2020-09-04')
+df[['Close']].plot(figsize=(12,12))
+# plt.show()
+
+#Open a new excel Workbook
+book = xw.Book()
+
+#Access the first sheet
+sheet = book.sheets[0]
+#rename the name of the sheet
+sheet.name = 'Week#1'
+
+# Write a panda data-frame to excel
+sheet['A1'].value = df
+
+# Save the excel
+book.save(r'/Users/srmk/Documents/Srikanth/nse-data/stocks/NSEI.xlsx')
+book.close()
