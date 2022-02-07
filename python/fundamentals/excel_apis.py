82da9d6403cda81341705ca03b4e7498b7e168d3
srmk <srikanth.mk@gmail.com>
2022-02-07 14:56:55 +0530

    Update excel_apis.py

--- python/fundamentals/excel_apis.py
+++ python/fundamentals/excel_apis.py
@@ -2,46 +2,51 @@ import yfinance as yf
 import pandas as pd
 import matplotlib.pyplot as plt
 import xlwings as xw
+import datetime
 
+# Add linechart to the sheet
+def addLineChart(sheet):
+    chart = sheet.charts.add()
+    chart.set_source_data(sheet.range('E2').expand())
+    chart.chart_type = 'line'
+    chart.name = 'Close'
 
-# References
-# https://www.dataquest.io/blog/python-excel-xlwings-tutorial/
-# https://www.xlwings.org/
+# Add new sheet to the book
+def addSheet(book, sheetName, dataFrame):
+    sheet = book.sheets.add()
+    sheet.name = sheetName
+    # Write a panda data-frame to excel
+    sheet['A1'].value = dataFrame
+    return sheet
 
-power = yf.Ticker("^NSEI")
-df = power.history(start="2020-01-01", end='2020-09-04')
-df[['Close']].plot(figsize=(12,12))
-# plt.show()
+# Save the excel
+def saveBook(book):
+    book.save(r'/Users/srmk/Documents/Srikanth/nse-data/stocks/NSEI.xlsx')
 
+# Read from the sheet to a dataFrame
+def readFromSheet(sheet):
+    data = sheet['A1'].expand().options(pd.DataFrame).value
+    return data
+
+nifty = yf.Ticker("^NSEI")
+day1 = datetime.datetime.strptime("2021-01-01","%Y-%m-%d")
+seven_days = datetime.timedelta(days=7)
 #Open a new excel Workbook
 book = xw.Book()
 
-#Access the first sheet
-sheet = book.sheets[0]
-#rename the name of the sheet
-sheet.name = 'Week#1'
-
-# Write a panda data-frame to excel
-sheet['A1'].value = df
-
-# Read 52 week OCHL and write it to excel
-day1 = datetime.datetime.strptime("2021-01-01","%Y-%m-%d")
-seven_days = datetime.timedelta(days=7)
 start_date = day1
 end_date = day1 + seven_days
-for i in range(1, 52):
+
+for i in range(1, 2):
     df = nifty.history(start=start_date, end=end_date, interval="1h")
     name = "Week#" + str(i)
-    sheet = book.sheets.add()
-    sheet.name = name
-    # Write a panda data-frame to excel
-    sheet['A1'].value = df
+    sheet = addSheet(book, name, df)
     start_date = end_date
     end_date = end_date + seven_days
+    addLineChart(sheet)
 
 # Read the data back to frame
-data = sheet['A1'].expand().options(pd.DataFrame).value
+data = readFromSheet(sheet)
+print(data)
 
-# Save the excel
-book.save(r'/Users/srmk/Documents/Srikanth/nse-data/stocks/NSEI.xlsx')
 book.close()
