636b1ce4c5887d6aa89089fddb57525e7e30b9a2
srmk <srikanth.mk@gmail.com>
2022-02-07 14:30:34 +0530

    Update excel_apis.py

--- python/fundamentals/excel_apis.py
+++ python/fundamentals/excel_apis.py
@@ -23,6 +23,24 @@ sheet.name = 'Week#1'
 # Write a panda data-frame to excel
 sheet['A1'].value = df
 
+# Read 52 week OCHL and write it to excel
+day1 = datetime.datetime.strptime("2021-01-01","%Y-%m-%d")
+seven_days = datetime.timedelta(days=7)
+start_date = day1
+end_date = day1 + seven_days
+for i in range(1, 52):
+    df = nifty.history(start=start_date, end=end_date, interval="1h")
+    name = "Week#" + str(i)
+    sheet = book.sheets.add()
+    sheet.name = name
+    # Write a panda data-frame to excel
+    sheet['A1'].value = df
+    start_date = end_date
+    end_date = end_date + seven_days
+
+# Read the data back to frame
+data = sheet['A1'].expand().options(pd.DataFrame).value
+
 # Save the excel
 book.save(r'/Users/srmk/Documents/Srikanth/nse-data/stocks/NSEI.xlsx')
 book.close()
