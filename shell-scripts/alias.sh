0a95a3d589c00ececbe5bd8cc87435eb38646c9b
srmk <srikanth.mk@gmail.com>
2022-11-25 16:37:19 +0530

    Create alias.sh

---
+++ shell-scripts/alias.sh
@@ -0,0 +1,10 @@
+#my alias
+alias t='telnet'
+alias f='finger'
+alias s='source'
+
+#my alaises
+#sometimes instead of aliases i used parameterised bash functions
+set-ddts() {
+    export DDTS=$1
+}
